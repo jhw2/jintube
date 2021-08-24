be9709c9ee6b13439c6523b1d30bf0b83752d378
Jin HyeWon <45846790+jhw2@users.noreply.github.com>
2021-08-24 16:48:30 +0900

    Update NavBar.jsx

--- client/src/components/views/NavBar/NavBar.jsx
+++ client/src/components/views/NavBar/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = ()=> {
   return (
     <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
       <div className="menu__logo">
-        <a href="/">Logo</a>
+        <a href="/">Jintube</a>
       </div>
       <div className="menu__container">
         <div className="menu_left">
