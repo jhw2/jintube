df1eb75e8812bd5bc6e2a27e9d92f4ef9fe450ef
hope5107@naver.com <hope5107@naver.com>
2021-10-25 13:21:10 +0900

    업로드서버 개발/운영 url env 적용

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,4 @@
 export const USER_SERVER = 'api/users';
 
 //SERVER URL
-export const SERVER_URL = 'http://localhost:5000/';
-//export const SERVER_URL = 'https://jintube-api-app.herokuapp.com/';
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
